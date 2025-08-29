4aae02d73148fb96fe90f1f2bd752b178fb5db29
Code-Vent <oyeyiolahamid@yahoo.com>
2025-08-29 18:12:56 +0100

    blinky

--- src/stm32f103c8/core/handlers.zig
+++ src/stm32f103c8/core/handlers.zig
@@ -1,6 +1,6 @@
 pub var systick: u32 = 0;
 const systick_ptr: *volatile u32 = &systick;
 
-export fn SysTick_Handler() void {
+export fn SysTick_Handler() callconv(.C) void {
     systick_ptr.* += 1;
 }
