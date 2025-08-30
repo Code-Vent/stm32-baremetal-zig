1d13b8a3e0dc08a81049ea18a8c90c4e5b3ff8a6
Code-Vent <oyeyiolahamid@yahoo.com>
2025-08-30 06:24:58 +0100

    blinky

--- src/stm32f103c8/core/core.zig
+++ src/stm32f103c8/core/core.zig
@@ -1,34 +1,34 @@
-const Organs = struct {
+const Units = struct {
     pub const clock = @import("clock.zig");
     pub const handlers = @import("handlers.zig");
     pub const sys_timer = @import("sys_timer.zig");
 };
 
-const Vitals = struct {
+const Env = struct {
     pub var clock_freq: u32 = 8_000_000;
-    pub const systick_ptr: *volatile u32 = &Organs.handlers.systick;
+    pub const systick_ptr: *volatile u32 = &Units.handlers.systick;
 };
 
 pub fn start(comptime freq_in_MHZ: u8) void {
-    const Config = Organs.clock.Config;
-    Vitals.clock_freq = Organs.clock.start(Config.init(freq_in_MHZ));
+    const Config = Units.clock.Config;
+    Env.clock_freq = Units.clock.start(Config.init(freq_in_MHZ));
 }
 
-pub fn enable_peripheral(clock: Organs.clock.ClockSrc, mask: u32) void {
-    Organs.clock.enable(clock, mask);
+pub fn enable_peripheral(clock: Units.clock.ClockSrc, mask: u32) void {
+    Units.clock.enable(clock, mask);
 }
 
 pub fn delay_ms(time: u32) void {
-    Organs.sys_timer.init_delay(Vitals.clock_freq, 1000);
+    Units.sys_timer.init_delay(Env.clock_freq, 1000);
     delay(time);
 }
 
 pub fn delay_us(time: u32) void {
-    Organs.sys_timer.init_delay(Vitals.clock_freq, 1000_000);
+    Units.sys_timer.init_delay(Env.clock_freq, 1000_000);
     delay(time);
 }
 
 inline fn delay(time: u32) void {
-    const start_time: u32 = Vitals.systick_ptr.*;
-    while ((Vitals.systick_ptr.* - start_time) < time) {}
+    const start_time: u32 = Env.systick_ptr.*;
+    while ((Env.systick_ptr.* - start_time) < time) {}
 }
--- src/stm32f103c8/peripherals/timers.zig
+++ src/stm32f103c8/peripherals/timers.zig
@@ -1,1 +1,5 @@
+const core = @import("../core/core.zig");
+
+//pub const Timers = union(enum) { A = 2, B = 3, C = 4, D = 5 };
+
 pub fn init() void {}
