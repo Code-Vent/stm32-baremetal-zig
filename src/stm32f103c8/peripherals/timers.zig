60d8f03a30a599b08073713b85d103866663a3e5
Code-Vent <oyeyiolahamid@yahoo.com>
2025-08-30 17:35:33 +0100

    timer

--- src/stm32f103c8/peripherals/timers.zig
+++ src/stm32f103c8/peripherals/timers.zig
@@ -1,5 +1,15 @@
 const core = @import("../core/core.zig");
+const timN = @import("timer_backend.zig");
 
-//pub const Timers = union(enum) { A = 2, B = 3, C = 4, D = 5 };
+pub const Config = timN.Config;
+pub const Timer = timN.Timer;
 
-pub fn init() void {}
+pub fn start_timer(comptime timer: timN.Timer) timN.TimerOptions {
+    timN.start(timer, timer);
+    return timer;
+}
+
+pub fn init() void {
+    const tim = Timer{ .TIM1 = Config{} };
+    start_timer(tim);
+}
